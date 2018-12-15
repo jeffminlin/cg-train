b624c82b17220dfeef2dd99fbe926af9c0f896d6
jeffminlin <35585117+jeffminlin@users.noreply.github.com>
2018-12-15 13:39:19 -0800

    added symmetry tests for log cosh activation

--- tests/test_model.py
+++ tests/test_model.py
@@ -21,10 +21,10 @@ def test_z2_sym_L8():
     assert_allclose(test_out, test_out_flip, rtol=1.0e1)
 
 
-def test_z2_sym_L4_log_cosh():
+def test_z2_sym_L8_log_cosh():
 
     config = tr.Config()
-    config.L = 4
+    config.L = 8
     config.conv_activ = 'log_cosh'
     config.refresh_config()
     deep_conv = tr.ConvIsing(config)
@@ -53,7 +53,24 @@ def test_translate_sym_L8():
     assert_allclose(test_out, test_out_roll, rtol=1.0e-4)
 
 
-def test_translate_sym_L8():
+def test_translate_sym_L8_log_cosh():
+
+    config = tr.Config()
+    config.L = 8
+    config.conv_activ = 'log_cosh'
+    config.refresh_config()
+    deep_conv = tr.ConvIsing(config)
+    deep_conv.reload_weights(config)
+
+    test_input = np.random.choice([-1, 1], size=(50, config.cgL, config.cgL))
+    test_input_roll = np.roll(test_input, np.random.randint(config.cgL, size=2), axis=(1, 2))
+    test_out = deep_conv.model_energy.predict(test_input)
+    test_out_roll = deep_conv.model_energy.predict(test_input_roll)
+
+    assert_allclose(test_out, test_out_roll, rtol=1.0e-4)
+
+
+def test_d4_sym_L8():
 
     config = tr.Config()
     config.L = 8
@@ -67,3 +84,19 @@ def test_translate_sym_L8():
     test_out_rot = deep_conv.model_energy.predict(test_input_rot)
 
     assert_allclose(test_out, test_out_rot, rtol=1.0e1)
+
+def test_d4_sym_L8_log_cosh():
+
+    config = tr.Config()
+    config.L = 8
+    config.conv_activ = 'log_cosh'
+    config.refresh_config()
+    deep_conv = tr.ConvIsing(config)
+    deep_conv.reload_weights(config)
+
+    test_input = np.random.choice([-1, 1], size=(50, config.cgL, config.cgL))
+    test_input_rot = np.array([np.rot90(ss) for ss in test_input])
+    test_out = deep_conv.model_energy.predict(test_input)
+    test_out_rot = deep_conv.model_energy.predict(test_input_rot)
+
+    assert_allclose(test_out, test_out_rot, rtol=1.0e-2)
