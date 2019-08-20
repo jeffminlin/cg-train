d46f14557a783cd536813fae5c4a4893cbd779fd
Jeffmin Lin <jeffminlin@berkeley.edu>
2019-08-20 11:55:42 -0700

    removed multigpucheckpoint in favor of tf strategy

--- convising/train.py
+++ convising/train.py
@@ -108,117 +108,6 @@ class Config:
         ).format(self.cgL, self.beta)
 
 
-class MultiGPUCheckpoint(tf.keras.callbacks.Callback):
-    """Save the model, with multiple GPUs (see ModelCheckpoint).
-
-    Args:
-        filepath (str): File of saved weights.
-        base_model (Model): Keras Model whose weights are being saved.
-        monitor (str): Metric to monitor.
-        verbose: Whether or not to print information each epoch.
-            Can be 0, 1 or 2. Defaults to 0.
-        save_best_only (bool): Whether to save the best weights or the most
-            recent weights. Defaults to False.
-        save_weights_only (bool): Whether to save just the weights or the
-            network graph as well. Defaults to False.
-        mode (str): If 'save_best_only' is True, decides to save new weights
-            based on when the monitored metric is maximized or minimized.
-            Defaults to 'auto'.
-        period (int): Number of epochs between checkpoints.
-
-    """
-
-    def __init__(
-        self,
-        filepath,
-        base_model,
-        monitor="val_loss",
-        verbose=0,
-        save_best_only=False,
-        save_weights_only=False,
-        mode="auto",
-        period=1,
-    ):
-        super(MultiGPUCheckpoint, self).__init__()
-        self.base_model = base_model
-        self.monitor = monitor
-        self.verbose = verbose
-        self.filepath = filepath
-        self.save_best_only = save_best_only
-        self.save_weights_only = save_weights_only
-        self.period = period
-        self.epochs_since_last_save = 0
-
-        if mode not in ["auto", "min", "max"]:
-            warnings.warn(
-                "ModelCheckpoint mode %s is unknown, "
-                "fallback to auto mode." % (mode),
-                RuntimeWarning,
-            )
-            mode = "auto"
-
-        if mode == "min":
-            self.monitor_op = np.less
-            self.best = np.Inf
-        elif mode == "max":
-            self.monitor_op = np.greater
-            self.best = -np.Inf
-        else:
-            if "acc" in self.monitor or self.monitor.startswith("fmeasure"):
-                self.monitor_op = np.greater
-                self.best = -np.Inf
-            else:
-                self.monitor_op = np.less
-                self.best = np.Inf
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        self.epochs_since_last_save += 1
-        if self.epochs_since_last_save >= self.period:
-            self.epochs_since_last_save = 0
-            filepath = self.filepath.format(epoch=epoch + 1, **logs)
-            if self.save_best_only:
-                current = logs.get(self.monitor)
-                if current is None:
-                    warnings.warn(
-                        "Can save best model only with %s available, "
-                        "skipping." % (self.monitor),
-                        RuntimeWarning,
-                    )
-                else:
-                    if self.monitor_op(current, self.best):
-                        if self.verbose > 0:
-                            print(
-                                "Epoch %05d: %s improved from %0.5f to %0.5f,"
-                                " saving model to %s"
-                                % (
-                                    epoch + 1,
-                                    self.monitor,
-                                    self.best,
-                                    current,
-                                    filepath,
-                                )
-                            )
-                        self.best = current
-                        if self.save_weights_only:
-                            self.base_model.save_weights(filepath, overwrite=True)
-                        else:
-                            self.base_model.save(filepath, overwrite=True)
-                    else:
-                        if self.verbose > 0:
-                            print(
-                                "Epoch %05d: %s did not improve"
-                                % (epoch + 1, self.monitor)
-                            )
-            else:
-                if self.verbose > 0:
-                    print("Epoch %05d: saving model to %s" % (epoch + 1, filepath))
-                if self.save_weights_only:
-                    self.base_model.save_weights(filepath, overwrite=True)
-                else:
-                    self.base_model.save(filepath, overwrite=True)
-
-
 def coarse_grain(L, beta, cg_method, cgf, image):
 
     image = image.reshape([-1, L, L])
@@ -588,7 +477,7 @@ class ConvIsing:
             for layer in self.model_energy.layers:
                 layer.trainable = True
 
-        best_weight = MultiGPUCheckpoint(
+        best_weight = tf.keras.callbacks.ModelCheckpoint(
             weightfile,
             self.model_energy,
             monitor="val_loss",
